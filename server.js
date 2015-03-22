21101341d6f0a5b73a533e9104bec2f4dcbadb84
unknown <aceeristudios@gmail.com>
2015-03-22 10:51:57 -0700

    Deploying application

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 var express = require('express');
 var fs      = require('fs');
 var qs      = require('querystring');
+require('newrelic');
 
 var ipaddress 	= process.env.OPENSHIFT_NODEJS_IP;
 var port 		= process.env.OPENSHIFT_NODEJS_PORT || 8080;
