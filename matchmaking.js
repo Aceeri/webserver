5c7c4698f6d82f4801eae15df8f6573ad48ff559
unknown <aceeristudios@gmail.com>
2015-03-28 01:52:32 -0700

    matchmaking.js

---
+++ matchmaking.js
@@ -0,0 +1,153 @@
+
+var matchmaking 	= { };
+
+//queues
+matchmaking.queue 	= [ ];
+var confirm 		= { };
+var arenas			= { };
+
+//polling arrays
+var confirmRequests = [ ];
+var acceptRequests 	= [ ];
+
+matchmaking.inQueue = function(userId, splice) {
+	for (i = 0; i < queue.length; i ++) {
+		if (queue[i].id == userId) {
+			if (splice) {
+				queue.splice(i, 1);
+			}
+			return true;
+		}
+	}
+}
+
+matchmaking.add = function(req, res) {
+	var userId = parseInt(req.body.id);
+	confirm[userId] = undefined;
+
+	if (!matchmaking.inQueue(userId)) {
+		queue.push({
+			name 	: req.body.name,
+			id 		: userId,
+			placeid : parseInt(req.body.placeid),
+			rank 	: parseInt(req.body.rank),
+			type 	: req.body.type
+		});
+		res.send('added');
+	} else {
+		res.send('rejected')
+	}
+}
+
+matchmaking.leave = function(req, res) {
+	var id = parseInt(req.params.id);
+	matchmaking.inQueue(id, true);
+
+	for (i = 0; i < confirmRequests.length; i++) {
+		if (parseInt(confirmRequests[i].request.params.id) == id) {
+			confirmRequests[i].response.end('left');
+			confirmRequests.splice(i, 1);
+		}
+	}
+
+	res.send("removed");
+}
+
+matchmaking.accept = function(req, res) {
+	var response = parseInt(req.body.response);
+	var userId   = parseInt(req.body.userId);
+
+	if (confirm[userId].players[0].id == userId) {
+		confirm[userId].players[0].accept = response;
+	} else if (confirm[userId].players[1].id == userId) {
+		confirm[userId].players[1].accept = response;
+	}
+
+	res.send("accept");
+}
+
+matchmaking.confirm = function(req, res) {
+	confirmRequests.push({
+		request 	: req,
+		response	: res,
+		timestamp	: new Date().getTime()
+	});
+}
+
+matchmaking.accepted = function(req, res) {
+	acceptRequests.push({
+		request 	: req,
+		response	: res,
+		timestamp	: new Date().getTime()
+	});
+}
+
+setInterval(function() {
+	var expiration 			= new Date().getTime() - 28000;
+	var acceptExpiration 	= new Date().getTime() - 20000;
+
+	for (p1 = 0; p1 < queue.length; p1++) {
+		for (p2 = 0; p2 < queue.length; p2 ++) {
+			var player1 = queue[p1];
+			var player2 = queue[p2];
+			if (player1.type == player2.type && player1.id != player2.id && Math.abs(player1.rank - player2.rank) <= 50) {
+				queue.splice((p1 > p2) ? p1 : p2, 1);
+				queue.splice((p1 > p2) ? p2 : p1, 1);
+				confirm[player1.id] = { players : [ player1, player2 ], id : player1.placeid, type : player1.type };
+				confirm[player2.id] = confirm[player1.id];
+			}
+		}
+	}
+
+	var response;
+	for (var i = confirmRequests.length - 1; i >= 0; i--) {
+		response = confirmRequests[i].response;
+		if (confirm[parseInt(confirmRequests[i].request.params.id)] != undefined) {
+			response.write(JSON.stringify(confirm[parseInt(confirmRequests[i].request.params.id)]));
+			response.end();
+			confirmRequests.splice(i, 1);
+		} else if (confirmRequests[i].timestamp < expiration) {
+			response.end('');
+		}
+	}
+
+	for (var i = acceptRequests.length - 1; i >= 0; i--) {
+		response = acceptRequests[i].response;
+		var set = true;
+		var canceled = false;
+		var playerIndex;
+		var request = confirm[parseInt(acceptRequests[i].request.params.id)];
+		if (request != undefined) {
+			for (var l = 0; l < request.players.length; l ++) {
+				if (request.players[l].accept == undefined) {
+					set = false;
+				} else if (request.players[l].accept == 2) {
+					canceled = true;
+					if (request.players[l].id == parseInt(acceptRequests[i].request.params.id)) {
+						playerIndex = l;
+					}
+				}
+			}
+
+		
+			if (set && !canceled) {
+				response.write('accepted');
+				response.end();
+				confirm[parseInt(acceptRequests[i].request.params.id)] = undefined;
+				acceptRequests.splice(i, 1);
+			} else if (acceptRequests[i].timestamp < acceptExpiration || canceled) {
+				if (request.players[playerIndex].accept == 2 || request.players[playerIndex].accept == 0) {
+					response.end('');
+				} else {
+					queue.push(request.players[playerIndex]);
+					response.write('added');
+					response.end('');
+				}
+				confirm[parseInt(acceptRequests[i].request.params.id)] = undefined;
+				acceptRequests.splice(i, 1);
+			}
+		}
+	}
+}, 1000);
+
+return matchmaking;
--- server.js
+++ server.js
@@ -1,193 +1,44 @@
 #!/bin/env node
-require('newrelic');
+//require('newrelic');
 var express = require('express');
-var fs      = require('fs');
-var qs      = require('querystring');
+var matchmaking = require('./matchmaking');
 
 var ipaddress 	= process.env.OPENSHIFT_NODEJS_IP;
 var port 		= process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var queue 		= [ ];
-var confirm 	= { };
-var arenas		= { };
 
 if (typeof ipaddress === "undefined") {
 	ipaddress = "127.0.0.1";
 };
 
 var app  = express();
+app.use(express.bodyParser());
+
 var id   = app.param("/^\d+$/");
 var type = app.param("/^\w+$/");
 
-app.use(express.bodyParser());
+var postRequest = [
+	{'/join', matchmaking.add},
+	{'/accept', matchmaking.accept}
+];
+var getRequest 	= [
+	{'/', function(req, res) { res.send(''); }); },
+	//{'/queue', function(req, res) { res.send(matchmaking.queue) }); },
+	{'/leave/:id', matchmaking.leave},
 
-app.post('/join', function(req, res) {
-	var userId = parseInt(req.body.id);
-	var found = false;
-	for (i = 0; i < queue.length; i ++) {
-		if (queue[i].id == userId) {
-			found = true;
-			break;
-		}
-	}
+	//polling
+	{'/confirm/:id', matchmaking.confirm},
+	{'/accept/:id', matchmaking.accept}
+];
 
-	confirm[userId] = undefined;
-
-	if (!found) {
-		queue.push({
-			name 	: req.body.name,
-			id 		: userId,
-			placeid : parseInt(req.body.placeid),
-			rank 	: parseInt(req.body.rank),
-			type 	: req.body.type
-		});
-		res.send("added");
-	} else {
-		res.send("rejected");
-	}
-});
-
-app.get('/leave/:id', function(req, res) {
-	var id = parseInt(req.params.id);
-	for (player = 0; player < queue.length; player++) {
-		if (queue[player].id == id) {
-			queue.splice(player, 1);
-			break;
-		}
-	}
-
-	for (i = 0; i < confirmRequests.length; i ++) {
-		if (parseInt(confirmRequests[i].request.params.id) == id) {
-			confirmRequests[i].response.end('left');
-			confirmRequests.splice(i, 1);
-		}
-	}
-
-	res.send("removed");
-});
-
-app.get('/confirm/remove/:id', function(req, res) {
-	confirm[parseInt(req.params.id)] = undefined;
-	res.end('removed');
-});
-
-var confirmRequests = [ ];
-app.get('/confirm/:id', function(req, res) {
-	confirmRequests.push({
-		request 	: req,
-		response	: res,
-		timestamp	: new Date().getTime()
-	});
-});
-
-var acceptRequests = [ ];
-app.get('/accept/:id', function(req, res) {
-	acceptRequests.push({
-		request 	: req,
-		response	: res,
-		timestamp	: new Date().getTime()
-	});
-});
-
-//check every second for response
-setInterval(function() {
-	var expiration 			= new Date().getTime() - 28000;
-	var acceptExpiration 	= new Date().getTime() - 20000;
-
-	for (p1 = 0; p1 < queue.length; p1++) {
-		for (p2 = 0; p2 < queue.length; p2 ++) {
-			var player1 = queue[p1];
-			var player2 = queue[p2];
-			if (player1.type == player2.type && player1.id != player2.id && Math.abs(player1.rank - player2.rank) <= 50) {
-				queue.splice((p1 > p2) ? p1 : p2, 1);
-				queue.splice((p1 > p2) ? p2 : p1, 1);
-				confirm[player1.id] = { players : [ player1, player2 ], id : player1.placeid, type : player1.type };
-				confirm[player2.id] = confirm[player1.id];
-			}
-		}
-	}
-
-	var response;
-	for (var i = confirmRequests.length - 1; i >= 0; i--) {
-		response = confirmRequests[i].response;
-		if (confirm[parseInt(confirmRequests[i].request.params.id)] != undefined) {
-			response.write(JSON.stringify(confirm[parseInt(confirmRequests[i].request.params.id)]));
-			response.end();
-			confirmRequests.splice(i, 1);
-		} else if (confirmRequests[i].timestamp < expiration) {
-			response.end('');
-		}
-	}
-
-	for (var i = acceptRequests.length - 1; i >= 0; i--) {
-		response = acceptRequests[i].response;
-		var set = true;
-		var canceled = false;
-		var playerIndex;
-		var request = confirm[parseInt(acceptRequests[i].request.params.id)];
-		if (request != undefined) {
-			for (var l = 0; l < request.players.length; l ++) {
-				if (request.players[l].accept == undefined) {
-					set = false;
-				} else if (request.players[l].accept == 2) {
-					canceled = true;
-					if (request.players[l].id == parseInt(acceptRequests[i].request.params.id)) {
-						playerIndex = l;
-					}
-				}
-			}
-
-		
-			if (set && !canceled) {
-				response.write('accepted');
-				response.end();
-				confirm[parseInt(acceptRequests[i].request.params.id)] = undefined;
-				acceptRequests.splice(i, 1);
-			} else if (acceptRequests[i].timestamp < acceptExpiration || canceled) {
-				if (request.players[playerIndex].accept == 2 || request.players[playerIndex].accept == 0) {
-					response.end('');
-				} else {
-					queue.push(request.players[playerIndex]);
-					response.write('added');
-					response.end('');
-				}
-				confirm[parseInt(acceptRequests[i].request.params.id)] = undefined;
-				acceptRequests.splice(i, 1);
-			}
-		}
-	}
-}, 1000);
-
-app.post('/accept', function(req, res) {
-	var response = parseInt(req.body.response);
-	var userId   = parseInt(req.body.userId);
-	if (confirm[userId].players[0].id == userId) {
-		confirm[userId].players[0].accept = response;
-	} else if (confirm[userId].players[1].id == userId) {
-		confirm[userId].players[1].accept = response;
-	}
-
-	res.send("accept");
-})
-
-app.get('/arenas/remove/:id', function(req, res) {
-	arenas[parseInt(req.params.id)] = undefined;
-	res.send("");
-});
+for (int i = 0; i < postRequest.length; i++) {
+	app.post(postRequest[0], postRequest[1]);
+}
 
 app.get('/arenas/:id', function(req, res) {
-	console.log(arenas[parseInt(req.params.id)] || "");
-	res.send(arenas[parseInt(req.params.id)] || "");
-	//test
+	var id = parseInt(req.params.id);
+	console.log(id)] || "");
+	res.send(arenas[id] || "");
+	arenas[id] = undefined;
 });
 
-app.get('/queue', function(req, res) {
-	res.send(queue);
-})
-
-app.get('/', function(req, res) {
-	res.send('');
-});
-
-app.listen(port, ipaddress, function() {
-	console.log("Server online.");
-});
+app.listen(port, ipaddress, function() { console.log("Online"); });
