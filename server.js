1322f267ac45624c4993d7c2e12be042da1e94a0
unknown <aceeristudios@gmail.com>
2015-03-28 03:54:19 -0700

    test

--- server.js
+++ server.js
@@ -2,7 +2,6 @@
 //require('newrelic');
 var express = require('express');
 var matchmaking = require('./matchmaking');
-console.log(matchmaking);
 
 var ipaddress 	= process.env.OPENSHIFT_NODEJS_IP;
 var port 		= process.env.OPENSHIFT_NODEJS_PORT || 8080;
